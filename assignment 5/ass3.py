6ad80db927676a94c53e6a26c8c4f37e4e152f18
TheArcaneVamp <146957043+TheArcaneVamp@users.noreply.github.com>
2024-08-19 16:33:37 +0530

    ass3 file added with updated code

--- assignment 5/ass3.py
+++ assignment 5/ass3.py
@@ -1,5 +1,22 @@
 st = input("Enter a string:")
 if (" " in st):
     raise Exception("No spaces allowed!")
+if (len(st)<3 or len(st)>4):
+    raise Exception("Must be between 3 or 4 characters!")
 list = [*st]
-print(list)
+if (len(st)==3):
+    for i in st:
+        for j in st:
+            if j != i:
+                for k in st:
+                  if k != i and k!=j:
+                      print(i,j,k)
+if (len(st)==4):
+    for i in st:
+        for j in st:
+            if j != i:
+                for k in st:
+                    if k != i and k != j:
+                        for l in st:
+                            if l != k and l !=j and l != k:
+                                print(i,j,k,l)
